5a7e7a7e128a9363ba487071958ea0b6f8a5791a
Basiljini <basilj7thonnicka@gmail.com>
2026-01-22 20:43:17 +0000

    Updated about page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
     <main>
      <div className="flex flex-col-reverse md:flex-row justify-around min-h-screen sm:mt-20">
       <div className="flex-col items-center md:items-start bg-gray-50 sm:mt-20">
-         <p className="text-center font-bold text-6xl text-black-500 ">Hi, I'm<span className="text-blue-800"> Basil👋🏻</span></p>
+         <p className="text-center font-bold text-6xl text-black-500 ">Hi, I &apos;m<span className="text-blue-800"> Basil👋🏻</span></p>
         <h1 className="font-medium text-center md:text-left sm:text-2xl md:text-4xl text-black-500 mt-8">Software Developer {"  "}</h1>
         <h1 className="font-medium text-center md:text-left sm:text-2xl md:text-4xl text-black-500">Aspiring Cloud Engineer & AI Enthusiast</h1>
         <div className="flex justify-center space-x-20 text-lg mt-20 sm:mb-20">
